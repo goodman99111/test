4925b58a13eac4ded2907a3e22148274633bfd5b
Visionary <goodman99111@gmail.com>
2018-07-22 17:42:35 +1100

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 //создаём объект приложения
 var app = express();
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT
 //определяем обработчик для маршрута "/"
 app.get("/", function(request, responce){
 
