15691b8f2c03daedb93dc8f4944e914b625f8570
Visionary <goodman99111@gmail.com>
2018-07-22 17:15:01 +1100

    index add

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require("express");
-//создаём объект приложени
+//создаём объект приложения
 var app = express();
 //определяем обработчик для маршрута "/"
 app.get("/", function(request, responce){
 
   //отправляет ответ
   responce.send("<h1>Hello</h1");
+});
+
+app.get("/about", function(request, responce){
+
+  responce.send("<h1>About site</h1>");
+});
+
+app.get("/contact", function(request, responce){
+
+  responce.send("<h1>Contact</h1>");
 });
 //Начинаем прослушивание подключения на 3000 порту
 app.listen(3000);
