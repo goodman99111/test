961cc52a3672fdd92d88cbf3141a6ebaccda59a3
Visionary <goodman99111@gmail.com>
2018-07-24 23:52:30 +1100

    add new main page

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 var express = require("express");
-//создаём объект приложения
+
 var app = express();
-//определяем обработчик для маршрута "/"
+
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(request, responce){
-
-  //отправляет ответ
-  responce.send("<h1>Andrew, сосамбе</h1");
+	
+	responce.send("<h1>Main page</h1>");
 });
-
-app.get("/about", function(request, responce){
-
-  responce.send("<h1>About site</h1>");
-});
-
 app.get("/contact", function(request, responce){
-
-  responce.send("<h1>Contact</h1>");
+	
+	responce.send("<h1>Контакты</h1>");
 });
-//Начинаем прослушивание подключения на 3000 порту
 app.listen(8080);
+
