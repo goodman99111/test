3076135027e8a92cfcb38a6c46e1d1a97650c91e
Visionary <goodman99111@gmail.com>
2018-07-29 00:48:16 +1100

    new main  page

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 app.use(express.static(__dirname + "/public"));
 
+app.post("/register", urlencodedParser, function(request, responce) {
+	if(!request.body) return responce.sendStatus(400);
+	console.log(request.body);
+ responce.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+
 app.get("/", function(request, responce){
-	
+
 	responce.send("<h1>Main page</h1>");
 });
 app.get("/contact", function(request, responce){
-	
+
 	responce.send("<h1>Контакты</h1>");
 });
 app.listen(8080);
-
