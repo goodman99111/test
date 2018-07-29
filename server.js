6059fb8eee9f2dbfe330410ab9389ab8b83faeb1
Visionary <goodman99111@gmail.com>
2018-07-29 20:42:10 +1100

    add socked.io

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require("body-parser");
-
-var app = express();
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.use(express.static(__dirname + "/public"));
+//app.use(http.static(__dirname + "/public"));
 
 app.post("/register", urlencodedParser, function(request, responce) {
 	if(!request.body) return responce.sendStatus(400);
@@ -14,10 +14,19 @@ app.post("/register", urlencodedParser, function(request, responce) {
 });
 
 app.get("/", function(request, responce){
-   
+	responce.sendfile(__dirname + "/public" + "/main.html");
 });
+//подключение пользователя
+io.on('connection', function (socket) {
+	console.log("new user connected");
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 app.get("/contact", function(request, responce){
 
 	responce.send("<h1>Контакты</h1>");
 });
-app.listen(8080);
+http.listen(8080);
