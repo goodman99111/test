962df68f62e85334530235974c5a840e12974433
Visionary <goodman99111@gmail.com>
2018-08-06 23:05:20 +1100

    Added the number of people on the site

--- server.js
+++ server.js
@@ -4,32 +4,31 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require("body-parser");
 
+
+var users_online = 0;
+
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static(__dirname + "/public"));
-
-app.post("/register", urlencodedParser, function(request, responce) {
-	if(!request.body) return responce.sendStatus(400);
-	console.log(request.body);
- responce.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-
 app.get("/", function(request, responce){
 	responce.sendfile(__dirname + "/public" + "/index.html");
 });
 //подключение пользователя
 io.on('connection', function (socket) {
-	console.log("new user connected"); //Уведомление о подключении клиента
-  socket.emit('news', { Server: 'connection complete ' }); //генерирует событие "news" и отправляет объект подключившемуся клиенту
-	socket.broadcast.emit('user connected', 'new user connected');//генерирует событие о подключении пользователя
+	users_online++;
+	console.log("new user connected. Users: " + users_online); //Уведомление о подключении клиента
+	socket.emit('news', { Server: 'connection complete ' }, users_online); //генерирует событие "news" и отправляет объект подключившемуся клиенту
+	//socket.broadcast.emit('userOnline', users_online);
+	socket.broadcast.emit('user connected', users_online);//генерирует событие о подключении пользователя
   socket.on('my other event', function (data) {  //Прослушивает событие "on", которое получает сообщение от клиента
     console.log(data);
 		socket.broadcast.emit('new msg', data);
   });
-});
 
-app.get("/contact", function(request, responce){
-
-	responce.send("<h1>Контакты</h1>");
+	socket.on('disconnect', function(reason){
+		users_online--;
+		console.log('User disconnect, reason: ' + reason + '. Users: ' + users_online);
+		socket.broadcast.emit('user disconnect', users_online);
+	});
 });
 http.listen(8080);
