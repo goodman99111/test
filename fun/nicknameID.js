170a8ed3cef19cbb33ef9f5d44dfa229d63bc359
Visionary <goodman99111@gmail.com>
2018-08-14 21:14:21 +1100

    online users

---
+++ fun/nicknameID.js
@@ -0,0 +1,43 @@
+//Массив ID[]-массив ID подключений
+//Массив Nick[]-массив ников
+//Id[0], Nick[0]- один пользователь
+
+//Заменяем рандомный ник на ник созданный пользователем
+function addNick(arrID, arrNick, id, nick){
+  var index = arrID.indexOf(id);
+  arrNick[index] = nick;
+  console.log("add nick: " + nick + ". ID: " + id)
+}
+//Добавляем новый ID и в соответствие ему рандомный ник
+function addID(arrID, arrNick, id){
+  arrID.push(id);
+  console.log("Add new ID: " + id);
+  arrNick.push(generateRandomNick(1,100000));
+}
+
+//Удаляем ID и соответсвующий ему ник
+function delID(arrID, arrNick, id){
+  var index = arrID.indexOf(id); // получаем индекс ID
+  arrID.splice(index, 1); //удаляем этот ID из массива
+  console.log("delete nickname: " + arrNick[index]);
+  arrNick.splice(index, 1);//удаляем ник соответствующий ID
+  console.log("delete ID: " + id);
+}
+
+//создаём рандомный никнейм
+function generateRandomNick(min, max){
+ var randomNick = "user" + Math.floor(Math.random() * (max - min + 1)) + min;
+ console.log('generate random nickname: ' + randomNick);
+ return randomNick;
+}
+
+function getNick(arrID, arrNick, id){
+  var index = arrID.indexOf(id);
+  return arrNick[index];
+}
+
+module.exports.delID = delID;
+module.exports.addID = addID;
+module.exports.addNick = addNick;
+module.exports.generateRandomNick = generateRandomNick;
+module.exports.getNick = getNick;
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require("body-parser");
+var sysNick = require("./fun/nicknameID");
 
 
 var users_online = 0;
-var users;
+var id = []; //ID подключившихся пользователей
+var nicknames = []; //Ники пользователей
+var number = 0;
+
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -17,19 +21,20 @@ app.get("/", function(request, responce){
 //подключение пользователя
 io.on('connection', function (socket) {
 	users_online++;
-	console.log("new user connected. Users: " + users_online); //Уведомление о подключении клиента
-	socket.emit('news', { Server: 'connection complete ' }, users_online); //генерирует событие "news" и отправляет объект подключившемуся клиенту
-	//socket.broadcast.emit('userOnline', users_online);
-	socket.broadcast.emit('user connected', users_online);//генерирует событие о подключении пользователя
-  socket.on('my other event', function (data) {  //Прослушивает событие "on", которое получает сообщение от клиента
+	console.log("new user connected. Users: " + users_online);
+	sysNick.addID(id, nicknames, socket.id); //Добавляем ID подключившегося в массив
+	socket.emit('news', { Server: 'connection complete ' }, users_online, sysNick.getNick(id, nicknames, socket.id), nicknames); //генерирует событие "news" и отправляет объект подключившемуся клиенту
+	socket.broadcast.emit('user connected', sysNick.getNick(id, nicknames, socket.id));//генерирует событие о подключении пользователя
+  socket.on('my other event', function (data) {  //Прослушивает событие , которое получает сообщение от клиента
     console.log(data);
 		socket.broadcast.emit('new msg', data);
   });
 
-	socket.on('disconnect', function(reason){
+socket.on('disconnect', function(reason){
 		users_online--;
 		console.log('User disconnect, reason: ' + reason + '. Users: ' + users_online);
-		socket.broadcast.emit('user disconnect', users_online);
+		socket.broadcast.emit('user disconnect', sysNick.getNick(id, nicknames, socket.id));
+		sysNick.delID(id, nicknames, socket.id);
 	});
 });
 http.listen(8080);
