1d083eee606848345de0d5d26944a4e5fef7445d
Visionary <goodman99111@gmail.com>
2018-08-15 20:21:17 +1100

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 
-var app = require('express')();
+var app = rpequire('express')();
 var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
