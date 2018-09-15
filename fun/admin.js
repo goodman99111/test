ea98d2d71bd2fa5763890e186a5e69339e1cbb50
Visionary <goodman99111@gmail.com>
2018-09-16 02:56:39 +1100

    new command

---
+++ fun/admin.js
@@ -0,0 +1,35 @@
+//уровни админов
+
+//бан
+function ban(){
+
+}
+
+//кик пользоватедя
+function kick(){
+
+}
+//вывести список юзеров
+function users(){
+
+}
+//бан по IP
+function banIP(){
+
+}
+//IP юзера
+function userIP(){
+
+}
+//Отправить сообщение юзеру
+function sendMessage(){
+
+}
+//Отправить сообщение всем юзерам в виде объявления
+function broadcastMessage(){
+
+}
+//Закрыть комнату
+function closeRoom(){
+
+}
